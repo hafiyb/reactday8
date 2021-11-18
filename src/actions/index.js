9b593b80c8b450a30707d16cfa6b78fa10217ff7
hafiyb <hafiyb@gmail.com>
2021-11-18 08:09:45 +0800

    changed http to https in api request

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,7 +8,7 @@ export const fetchMovie = (data) => (dispatch) => {
     })
 
     try{
-        axios.get(`http://www.omdbapi.com/?apikey=32d62dad&s=${data}&page=1`)
+        axios.get(`https://www.omdbapi.com/?apikey=32d62dad&s=${data}&page=1`)
         .then((res) => {
             console.log(res)
             if(res.status === 200 && res.data.Response == 'True'){
@@ -43,7 +43,7 @@ export const fetchMovieDetails = (data) => (dispatch)  =>{
     })
 
     try{
-        axios.get(`http://www.omdbapi.com/?apikey=32d62dad&i=${data}`)
+        axios.get(`https://www.omdbapi.com/?apikey=32d62dad&i=${data}`)
         .then((res) => {
             if(res.status == 200){
                 dispatch({
